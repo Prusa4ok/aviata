2d4d1817777f6f1ba2d163b6e2d3fcd355b8c75d
Ruslan Mokrov <mokrov.r@raketa.city>
2021-10-06 00:28:21 +0600

    structured files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <filter-flights />
+  </div>
+</template>
+
+<script>
+import FilterFlights from "./views/FilterFlights.vue";
+
+export default {
+  name: "App",
+  components: {
+    FilterFlights,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
